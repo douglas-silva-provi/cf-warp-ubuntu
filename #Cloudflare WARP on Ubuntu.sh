4e4606297bc30221dda79cdb9deb325ee4525767
Douglas Silva <83734724+douglas-silva-provi@users.noreply.github.com>
2021-05-24 21:14:07 -0300

    Add files via upload

---
+++ #Cloudflare WARP on Ubuntu.sh
@@ -0,0 +1,41 @@
+#Cloudflare WARP on Ubuntu
+#
+#
+#add repository
+    $ echo deb http://pkg.cloudflare.com/ <release ubuntu> main | sudo tee /etc/apt/sources.list.d/cloudflare-main.list
+#
+#check your release
+    $ lsb_release -d
+#
+#example add repository with release ubuntu 20.04
+    $ echo deb http://pkg.cloudflare.com/ focal main | sudo tee /etc/apt/sources.list.d/cloudflare-main.list
+#
+#
+#
+#import GPG Key
+    #install comando 'curl': $ sudo apt install curl
+
+        $ curl -c - https://pkg.cloudflare.com/pbkey.gpg | sudo apt-key add -
+#
+#
+#update cache
+    $ sudo apt-get update
+#
+#
+#
+#installation
+    $ sudo apt install cloudflare-warp
+#
+#
+#
+#principals commands on warp-cli
+    #first utilization, register to WARP
+        $ warp-cli register
+#
+    #after, connect to WARP for access apps to VPN connection
+        $ warp-cli connect
+#
+#see successful connection
+    $ curl https://www.cloudflare.com/cdn-cgi/trace/
+#
+#verify to warp=on is OK!
